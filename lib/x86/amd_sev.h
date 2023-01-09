d9c5f0dad8b169c19f93b1c0917b39bb4f46158b
Pavan Kumar Paluri <papaluri@amd.com>
2023-01-09 15:21:39 -0600

    x86 AMDSEV-SNP: CPUID leaf validation using GHCB page-state PACV
    Validate all CPUID leaves for all the sub-functions (zero and non-zero)
    fetched from hypervisor using GHCB requests against CPUID leaves
    obtained From 'cpuid' instruction (which in case of SEV-SNP guest comes
    from the SEV-SNP CPUID table).
    
    HUGH....Patience.. I think we are getting closer now.
    
    Signed-off-by: Pavan Kumar Paluri <papaluri@amd.com>

--- lib/x86/amd_sev.h
+++ lib/x86/amd_sev.h
@@ -38,6 +38,15 @@ struct cc_blob_sev_info {
 	u32 rsvd2;
 } __packed;
 
+struct cpuid_leaf {
+	uint32_t eax_in;
+	uint32_t ecx_in;
+	uint32_t eax;
+	uint32_t ebx;
+	uint32_t ecx;
+	uint32_t edx;
+};
+
 /*
  * AMD Programmer's Manual Volume 3
  *   - Section "Function 8000_0000h - Maximum Extended Function Number and Vendor String"
@@ -68,12 +77,113 @@ efi_status_t setup_amd_sev(void);
  *   - Section "#VC Exception"
  */
 #define SEV_ES_VC_HANDLER_VECTOR 29
+#define SVM_EXIT_CPUID  0x72ULL
 
 /*
  * AMD Programmer's Manual Volume 2
  *   - Section "GHCB"
  */
 #define SEV_ES_GHCB_MSR_INDEX 0xc0010130
+#define VMGEXIT()		{ asm volatile("rep; vmmcall\n\r"); }
+
+typedef struct {
+	uint8_t  reserved1[203];
+	uint8_t  cpl;
+	uint8_t  reserved8[300];
+	uint64_t rax;
+	uint8_t  reserved4[264];
+	uint64_t rcx;
+	uint64_t rdx;
+	uint64_t rbx;
+	uint8_t  reserved5[112];
+	uint64_t sw_exit_code;
+	uint64_t sw_exit_info1;
+	uint64_t sw_exit_info2;
+	uint64_t sw_scratch;
+	uint8_t  reserved6[56];
+	uint64_t xcr0;
+	uint8_t  valid_bitmap[16];
+	uint64_t x87_state_gpa;
+	uint8_t  reserved7[1016];
+} ghcb_save_area;
+
+typedef struct {
+	ghcb_save_area	save_area;
+	uint8_t		shared_buffer[2032];
+	uint8_t		reserved1[10];
+	uint16_t	protocol_version;
+	uint32_t	ghcb_usage;
+} ghcb_page;
+
+#define OFFSET_OF(TYPE, Field)  ((uint64_t)&(((TYPE *)0)->Field))
+
+#define GHCB_SAVE_AREA_QWORD_OFFSET(reg_field) \
+	(OFFSET_OF(ghcb_page, save_area.reg_field) / sizeof(uint64_t))
+
+typedef enum {
+	ghcb_cpl	= GHCB_SAVE_AREA_QWORD_OFFSET(cpl),
+	ghcb_rax	= GHCB_SAVE_AREA_QWORD_OFFSET(rax),
+	ghcb_rbx	= GHCB_SAVE_AREA_QWORD_OFFSET(rbx),
+	ghcb_rcx	= GHCB_SAVE_AREA_QWORD_OFFSET(rcx),
+	ghcb_rdx	= GHCB_SAVE_AREA_QWORD_OFFSET(rdx),
+	ghcb_xcr0	= GHCB_SAVE_AREA_QWORD_OFFSET(xcr0),
+	ghcb_sw_exit_code = GHCB_SAVE_AREA_QWORD_OFFSET(sw_exit_code),
+	ghcb_sw_exit_info1 = GHCB_SAVE_AREA_QWORD_OFFSET(sw_exit_info1),
+	ghcb_sw_exit_info2 = GHCB_SAVE_AREA_QWORD_OFFSET(sw_exit_info2),
+	ghcb_sw_scratch	= GHCB_SAVE_AREA_QWORD_OFFSET(sw_scratch),
+} GHCB_REGISTER;
+
+/* MSR information for SEV_ES_GHCB_MSR_INDEX */
+typedef union {
+	struct {
+		uint32_t function:  12;
+		uint32_t reserved1: 20;
+		uint32_t reserved2: 32;
+	} ghcb_info;
+
+	struct {
+		uint8_t reserved[3];
+		uint8_t sev_enc_bit_pos;
+		uint8_t sev_protocol_min;
+		uint8_t sev_protocol_max;
+	} ghcb_protocol;
+
+	struct {
+		uint32_t function       : 12;
+		uint32_t reason_code_set: 4;
+		uint32_t reason_code    : 8;
+		uint32_t reserved1      : 8;
+		uint32_t reserved2      : 32;
+	} ghcb_terminate;
+
+	struct {
+		uint64_t function  : 12;
+		uint64_t features  : 52;
+	} ghcb_hyp_features;
+
+	struct {
+		uint64_t function	: 12;
+		uint64_t guest_frame_no : 52;
+	} ghcb_gpa_reg;
+
+	struct {
+		uint64_t  function	: 12;
+		uint64_t  gfn		: 40;
+		uint64_t  operation	: 4;
+		uint64_t  reserved	: 8;
+	} snp_psc_req;
+
+	struct {
+		uint32_t    function : 12;
+		uint32_t    reserved : 20;
+		uint32_t    error_code;
+	} snp_psc_resp;
+
+
+	uint64_t ghcb_phys_addr;
+	void *ghcb;
+
+} msr_sev_ghcb_reg;
 
 bool amd_sev_es_enabled(void);
 void setup_ghcb_pte(pgd_t *page_table);
@@ -83,6 +193,12 @@ efi_status_t setup_vc_handler(void);
 
 unsigned long long get_amd_sev_c_bit_mask(void);
 unsigned long long get_amd_sev_addr_upperbound(void);
+void vmg_set_offset_valid(ghcb_page *ghcb, GHCB_REGISTER offset);
+void mem_fence(void);
+void vmgexit(ghcb_page *ghcb, uint64_t exit_code, uint64_t exit_info1,
+		uint64_t exit_info2);
+uint64_t asm_read_cr4(void);
+uint64_t asm_xgetbv(uint32_t index);
 
 #endif /* TARGET_EFI */
 
