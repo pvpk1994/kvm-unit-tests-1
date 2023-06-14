7a089fd927928f00d78235218f5217a4e62c3360
Pavan Kumar Paluri <papaluri@amd.com>
2023-06-14 10:14:41 -0500

    x86 AMD SEV-SNP: Add tests for presence of confidential computing blob
    Add support to enable search for confidential computing blob in the EFI
    config table for KVM-Unit-Tests. Initial setup of SEV-SNP relies on
    information provided by this confidential computing blob.
    
    Put out a warning message in case the CC blob is not found.
    
    Signed-off-by: Pavan Kumar Paluri <papaluri@amd.com>

--- x86/amd_sev.c
+++ x86/amd_sev.c
@@ -20,6 +20,8 @@
 
 #define TESTDEV_IO_PORT 0xe0
 
+struct cc_blob_sev_info *snp_cc_blob;
+
 static char st1[] = "abcdefghijklmnop";
 
 static int test_sev_activation(void)
@@ -69,12 +71,44 @@ static void test_sev_es_activation(void)
 	}
 }
 
+/* Check to find if SEV-SNP's Confidential Computing Blob is present */
+static efi_status_t find_cc_blob_efi(void)
+{
+	efi_status_t status;
+
+	status = efi_get_system_config_table(EFI_CC_BLOB_GUID,
+		 (void **)&snp_cc_blob);
+
+	if (status != EFI_SUCCESS)
+		return status;
+
+	if (!snp_cc_blob)
+		return EFI_NOT_FOUND;
+
+	if (snp_cc_blob->magic != CC_BLOB_SEV_HDR_MAGIC)
+		return EFI_UNSUPPORTED;
+
+	return EFI_SUCCESS;
+}
+
 static void test_sev_snp_activation(void)
 {
+	efi_status_t status;
+
 	if (rdmsr(MSR_SEV_STATUS) & SEV_SNP_ENABLED_MASK) {
 		printf("SEV-SNP is enabled.\n");
 	} else {
 		printf("SEV-SNP is not enabled.\n");
+	}
+
+	if (amd_sev_snp_enabled()) {
+		status = find_cc_blob_efi();
+
+		if (status != EFI_SUCCESS)
+			printf("%sSEV-SNP CC blob is %spresent.\n",
+			       "WARNING: ", "NOT ");
+	} else {
+		printf("WARNING: SEV-SNP is not enabled.\n");
 	}
 }
 
