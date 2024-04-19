38ea5a5f035909e7283a068c07883d8ebf3812f7
Pavan Kumar Paluri <papaluri@amd.com>
2024-04-19 06:32:54 -0500

    x86/apic: Include asm/io.h and use those definitions to avoid duplication
    To avoid duplication, use outb() definition from asm/io.h.
    
    No functional change has been introduced in this patch.
    
    Signed-off-by: Pavan Kumar Paluri <papaluri@amd.com>

--- lib/x86/apic.c
+++ lib/x86/apic.c
@@ -4,6 +4,7 @@
 #include "processor.h"
 #include "smp.h"
 #include "asm/barrier.h"
+#include "asm/io.h"
 
 /* xAPIC and I/O APIC are identify mapped, and never relocated. */
 static void *g_apic = (void *)APIC_DEFAULT_PHYS_BASE;
@@ -21,11 +22,6 @@ struct apic_ops {
 static struct apic_ops *get_apic_ops(void)
 {
 	return this_cpu_read_apic_ops();
-}
-
-static void outb(unsigned char data, unsigned short port)
-{
-	asm volatile ("out %0, %1" : : "a"(data), "d"(port));
 }
 
 void eoi(void)
