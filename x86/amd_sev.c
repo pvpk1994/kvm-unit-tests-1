0236b2bea28b37f865678d9907eeb82240b544f8
Pavan Kumar Paluri <papaluri@amd.com>
2024-04-19 06:36:19 -0500

    x86 AMD SEV-SNP: Test Private->Shared Page state changes using GHCB MSR
    As mentioned in the GHCB spec (Section 2.3.1 GHCB MSR protocol), the
    SEV-SNP guest VM issues 4K page state change requests to the hypervisor
    to convert KUT's newly allocated private pages to shared pages using
    GHCB MSR protcol.
    
    The purpose of this test is to determine whether the hypervisor changes
    the page state to shared. Before the conversion test, ensure the state
    of the pages are in an expected state (i.e., private) by issuing a
    re-validation on one of the newly allocated page to determine the
    expected state of the page matches with the page's current state.
    Report failure if the expected page state is not private.
    
    Import GHCB MSR PSC related definitions from upstream linux
    (arch/x86/include/asm/sev-common.h and arch/x86/include/asm/sev.h)
    
    Signed-off-by: Pavan Kumar Paluri <papaluri@amd.com>

--- x86/amd_sev.c
+++ x86/amd_sev.c
@@ -14,11 +14,14 @@
 #include "x86/processor.h"
 #include "x86/amd_sev.h"
 #include "msr.h"
+#include "x86/vm.h"
+#include "alloc_page.h"
 
 #define EXIT_SUCCESS 0
 #define EXIT_FAILURE 1
 
 #define TESTDEV_IO_PORT 0xe0
+#define SNP_PSC_ALLOC_ORDER 10
 
 static char st1[] = "abcdefghijklmnop";
 
@@ -94,6 +97,140 @@ static efi_status_t find_cc_blob_efi(void)
 	return EFI_SUCCESS;
 }
 
+static inline int pvalidate(u64 vaddr, bool rmp_size,
+			    bool validate)
+{
+	bool rmp_unchanged;
+	int result;
+
+	asm volatile(".byte 0xF2, 0x0F, 0x01, 0xFF\n\t"
+		     CC_SET(c)
+		     : CC_OUT(c) (rmp_unchanged), "=a" (result)
+		     : "a" (vaddr), "c" (rmp_size), "d" (validate)
+		     : "memory", "cc");
+
+	if (rmp_unchanged)
+		return PVALIDATE_FAIL_NOUPDATE;
+
+	return result;
+}
+
+static efi_status_t __sev_set_pages_state_msr_proto(unsigned long vaddr, int npages,
+						    int operation)
+{
+	unsigned long vaddr_end = vaddr + (npages * PAGE_SIZE);
+	unsigned long paddr;
+	int ret;
+	u64 val;
+
+	/*
+	 * We are re-using GHCB MSR value setup by OVMF, so save and
+	 * restore it after PSCs.
+	 */
+	phys_addr_t ghcb_old_msr = rdmsr(SEV_ES_GHCB_MSR_INDEX);
+
+	while (vaddr < vaddr_end) {
+		/*
+		 * Although identity mapped, compute GPA to use guest
+		 * physical frame number (GFN) while requesting an
+		 * explicit page state change.
+		 */
+		paddr = __pa(vaddr);
+
+		if (operation == SNP_PAGE_STATE_SHARED) {
+			/* Page invalidation happens before changing to shared */
+			ret = pvalidate(vaddr, RMP_PG_SIZE_4K, false);
+			if (ret) {
+				printf("Failed to invalidate vaddr: 0x%lx, ret: %d\n",
+				       vaddr, ret);
+				return ES_UNSUPPORTED;
+			}
+		}
+
+		wrmsr(SEV_ES_GHCB_MSR_INDEX,
+		      GHCB_MSR_PSC_REQ_GFN(paddr >> PAGE_SHIFT, operation));
+
+		VMGEXIT();
+
+		val = rdmsr(SEV_ES_GHCB_MSR_INDEX);
+
+		if (GHCB_RESP_CODE(val) != GHCB_MSR_PSC_RESP) {
+			printf("Wrong PSC response code: 0x%x\n",
+			       (unsigned int)GHCB_RESP_CODE(val));
+			return ES_VMM_ERROR;
+		}
+
+		if (GHCB_MSR_PSC_RESP_VAL(val)) {
+			printf("Failed to change page state to %s paddr: 0x%lx error: 0x%llx\n",
+			       operation == SNP_PAGE_STATE_PRIVATE ? "private"
+								   : "shared",
+			       paddr, GHCB_MSR_PSC_RESP_VAL(val));
+			return ES_VMM_ERROR;
+		}
+
+		if (operation == SNP_PAGE_STATE_PRIVATE) {
+			ret = pvalidate(vaddr, RMP_PG_SIZE_4K, true);
+			if (ret) {
+				printf("Failed to validate vaddr: 0x%lx, ret: %d\n",
+				       vaddr, ret);
+				return ES_UNSUPPORTED;
+			}
+		}
+
+		vaddr += PAGE_SIZE;
+	}
+
+	/* Restore old GHCB MSR - setup by OVMF */
+	wrmsr(SEV_ES_GHCB_MSR_INDEX, ghcb_old_msr);
+
+	return ES_OK;
+}
+
+static void set_pte_decrypted(unsigned long vaddr, int npages)
+{
+	pteval_t *pte;
+	unsigned long vaddr_end = vaddr + (npages * PAGE_SIZE);
+
+	while (vaddr < vaddr_end) {
+		pte = get_pte((pgd_t *)read_cr3(), (void *)vaddr);
+
+		if (!pte)
+			assert_msg(pte, "No pte found for vaddr 0x%lx", vaddr);
+
+		/* unset c-bit */
+		*pte &= ~(get_amd_sev_c_bit_mask());
+
+		vaddr += PAGE_SIZE;
+	}
+
+	flush_tlb();
+}
+
+static efi_status_t sev_set_pages_state_msr_proto(unsigned long vaddr, int npages,
+						  int operation)
+{
+	efi_status_t status;
+
+	vaddr = vaddr & PAGE_MASK;
+
+	/*
+	 * If the encryption bit is to be cleared, change the page state
+	 * in the RMP table.
+	 */
+	if (operation == SNP_PAGE_STATE_SHARED) {
+		status = __sev_set_pages_state_msr_proto(vaddr, npages,
+							 operation);
+		if (status != ES_OK) {
+			printf("Page state change (Private->Shared) failure.\n");
+			return status;
+		}
+
+		set_pte_decrypted(vaddr, npages);
+	}
+
+	return ES_OK;
+}
+
 static void test_sev_snp_activation(void)
 {
 	efi_status_t status;
@@ -107,6 +244,51 @@ static void test_sev_snp_activation(void)
 
 	status = find_cc_blob_efi();
 	report(status == EFI_SUCCESS, "SEV-SNP CC-blob presence");
+}
+
+/*
+ * Perform page revalidation to ensure page is in the expected private
+ * state. We can confirm this test to succeed when the pvalidate fails
+ * with a return code of PVALIDATE_FAIL_NOUPDATE.
+ */
+static bool is_validated_private_page(unsigned long vaddr, bool rmp_size,
+				      bool state)
+{
+	int ret;
+
+	/* Attempt a pvalidate here for the provided page size */
+	ret = pvalidate(vaddr, rmp_size, state);
+	if (ret == PVALIDATE_FAIL_NOUPDATE)
+		return true;
+
+	/*
+	 * If PVALIDATE_FAIL_SIZEMISMATCH, Entry in the RMP is a 4K
+	 * entry, and what guest is providing is a 2M entry. Therefore,
+	 * fallback to pvalidating 4K entries within 2M range.
+	 */
+	if (rmp_size && ret == PVALIDATE_FAIL_SIZEMISMATCH) {
+		unsigned long vaddr_end = vaddr + LARGE_PAGE_SIZE;
+
+		for (; vaddr < vaddr_end; vaddr += PAGE_SIZE) {
+			ret = pvalidate(vaddr, RMP_PG_SIZE_4K, state);
+			if (ret != PVALIDATE_FAIL_NOUPDATE)
+				return false;
+		}
+	}
+
+	return ret == PVALIDATE_FAIL_NOUPDATE ? true : false;
+}
+
+static int test_write(unsigned long vaddr, int npages)
+{
+	unsigned long vaddr_end = vaddr + (npages << PAGE_SHIFT);
+
+	while (vaddr < vaddr_end) {
+		memcpy((void *)vaddr, st1, strnlen(st1, PAGE_SIZE));
+		vaddr += PAGE_SIZE;
+	}
+
+	return 0;
 }
 
 static void test_stringio(void)
@@ -126,6 +308,60 @@ static void test_stringio(void)
 	report((got & 0xff00) >> 8 == st1[sizeof(st1) - 2], "outsb up");
 }
 
+static void test_sev_psc_ghcb_msr(void)
+{
+	pteval_t *pte;
+	unsigned long *vaddr;
+	efi_status_t status;
+
+	vaddr = alloc_pages(SNP_PSC_ALLOC_ORDER);
+	if (!vaddr)
+		assert_msg(vaddr, "Page allocation failure at addr: %p", vaddr);
+
+	/*
+	 * Page state changes using GHCB MSR protocol can only happen on
+	 * 4K pages.
+	 */
+	force_4k_page(vaddr);
+
+	/* Use this pte to check the C-bit */
+	pte = get_pte_level((pgd_t *)read_cr3(), (void *)vaddr, 1);
+	if (!pte) {
+		assert_msg(pte, "No pte found for vaddr %p", vaddr);
+		return;
+	}
+
+	if (*pte & get_amd_sev_c_bit_mask()) {
+		/*
+		 * Before performing private->shared test, ensure the
+		 * page is in private and in a validated state.
+		 */
+		report(is_validated_private_page((unsigned long)vaddr,
+						 RMP_PG_SIZE_4K, true),
+		       "Expected page state: Private");
+
+		report_info("Private->Shared conversion test using GHCB MSR");
+
+		/* Perform Private->Shared page state change */
+		status = sev_set_pages_state_msr_proto((unsigned long)vaddr,
+						       1 << SNP_PSC_ALLOC_ORDER,
+						       SNP_PAGE_STATE_SHARED);
+
+		report(status == ES_OK, "Private->Shared Page State Change");
+
+		/*
+		 * Access the now-shared page(s) with C-bit cleared and
+		 * ensure read/writes return expected data.
+		 */
+		report(!test_write((unsigned long)vaddr, 1 << SNP_PSC_ALLOC_ORDER),
+		       "Write to %d unencrypted pages after private->shared conversion",
+		       1 << SNP_PSC_ALLOC_ORDER);
+	}
+
+	/* Cleanup */
+	free_pages_by_order(vaddr, SNP_PSC_ALLOC_ORDER);
+}
+
 int main(void)
 {
 	int rtn;
@@ -134,5 +370,10 @@ int main(void)
 	test_sev_es_activation();
 	test_sev_snp_activation();
 	test_stringio();
+	setup_vm();
+
+	if (amd_sev_snp_enabled())
+		test_sev_psc_ghcb_msr();
+
 	return report_summary();
 }
